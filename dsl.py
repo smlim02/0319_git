b3db988e2d82c90b76ebee29606d8ca433adc6dd
minion <jssmlim02@gmail.com>
2023-03-19 15:41:23 +0900

    add chaewon

---
+++ dsl.py
@@ -0,0 +1,12 @@
+class dsl(object):    
+    def __init__(self,name):
+        self.name = name
+    def whois(self):
+        if self.name == 'soeun':
+            print(f'{self.name} is vice president')
+        elif self.name == "gunwoo":
+            print(f'{self.name} is president')
+        elif self.name == "chaewon":
+            print(f'{self.name} is hongboking')
+        else:
+            print(f'{self.name}이 누구임?')
